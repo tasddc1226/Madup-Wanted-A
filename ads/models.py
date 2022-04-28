de63db3e9a8869c157ff08483770c28f38b3620d
YUNSANGMIN <74187642+redtea89@users.noreply.github.com>
2022-04-28 21:59:40 +0900

    FIX: FK필드 _id 제거

--- ads/models.py
+++ ads/models.py
@@ -5,12 +5,12 @@ from advertisers.models import advertiser_info
 # Create your models here.
 class ad_info(TimeStamp):
     uid = models.CharField(max_length=100, primary_key=True)
-    advertiser_id = models.ForeignKey(advertiser_info,max_length=100, on_delete=models.CASCADE)
+    advertiser = models.ForeignKey(advertiser_info,max_length=100, on_delete=models.CASCADE)
     media = models.CharField(max_length=100)
     
 
 class result_data_set(models.Model):
-    advertiser_id = models.ForeignKey(advertiser_info,max_length=100, on_delete=models.CASCADE)
+    advertiser = models.ForeignKey(advertiser_info,max_length=100, on_delete=models.CASCADE)
     uid = models.ForeignKey(ad_info ,max_length=100, primary_key=True, on_delete=models.CASCADE)
     media = models.CharField(max_length=100)
     date = models.DateTimeField()
