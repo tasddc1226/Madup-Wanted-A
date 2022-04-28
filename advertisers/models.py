baac1f3b99d2696fa0788f3513be99febfdd970c
tasddc1226 <tasddc@naver.com>
2022-04-28 19:36:12 +0900

    FIX: advertiser_info 모델링 수정

--- advertisers/models.py
+++ advertisers/models.py
@@ -4,3 +4,6 @@ from core.models import TimeStamp
 # Create your models here.
 class advertiser_info(TimeStamp):
     advertiser_id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=50, default='')
+    email = models.CharField(max_length=255, default='')
+    phone = models.CharField(max_length=15, default='')
