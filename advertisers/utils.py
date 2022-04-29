b43ec9173c433900bae1b1e42283fda1ce1e15ff
fore0919 <gms02294@gmail.com>
2022-04-29 12:11:00 +0900

    ADD : 광고주 CRUD 추가

---
+++ advertisers/utils.py
@@ -0,0 +1,8 @@
+import re
+
+# 권은경
+def validate_email(email):
+    return re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
+
+def validate_phone(phone):
+    return re.match('\d{3}-\d{3,4}-\d{4}', phone)
